7e557b507cd74e10071b760c163e2a8bc4fa0155
BhanuPratap-Singh-Naruka <pratapbhanu0187@gmail.com>
2023-08-05 21:24:39 +0530

    feat: established redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+/* eslint-disable no-unused-vars */
 import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import { fetchData } from "./utils/api";
+import { getApiConfiguration } from "./store/homeSlice";
 
 function App() {
+	const dispatch = useDispatch();
+	const url = useSelector((state) => state.home.url);
+	console.log(url);
 	useEffect(() => {
 		apiTesting();
 	}, []);
 
 	const apiTesting = () => {
 		fetchData("/movie/popular").then((data) => {
-			console.log(data);
+			dispatch(getApiConfiguration(data));
 		});
 	};
 
